3605e051f4dc39756fcb7e6921f6b44a65c7a36c
Ololade Adeyemi <ssdigital013@gmail.com>
2021-04-10 22:21:36 +0100

    Began making each section of the homepage responsive. Stopped at 786px screen resolution for the amp and mic sections

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,28 @@
 /* DOM SELECTIONS */
 
 const navBar = document.querySelector('#navigation');
+const navBarLinks = document.querySelectorAll('#navigation div.nav-links-container a');
+
+
 
 /* FUNCTIONS */
 
+function changeNavBarColor () {
 
+    if (window.innerHeight === 625) {
+
+        // navBar.id = "navigationWhite";
+        // navBarLinks.id = "";
+            console.log("Window's inner height is 625");
+    } else {
+
+        navBar.id = "navigation";
+        navBarLinks = `Window's inner height is ${window.innerHeight}`;
+
+    }
+    
+}
 
 /* EVENT LISTENERS */
 
+window.addEventListener('mousewheel', changeNavBarColor);
