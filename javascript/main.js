b2feb34ab74b943845f98b2e4bc8743101a318bb
Ololade Adeyemi <ssdigital013@gmail.com>
2021-04-15 08:52:32 +0100

    ...

--- javascript/main.js
+++ javascript/main.js
@@ -5,25 +5,40 @@ const navBarLinks = document.querySelectorAll('#navigation div.nav-links-contain
 
 /* FUNCTIONS */
 
-function changeNavBarColor () {
-
-    if (window.innerHeight === 625) {
-
-        // navBar.id = "navigationWhite";
-        // navBarLinks.id = "";
-        console.log("Window's inner height is 625");
-        
-    } else {
-
-        navBar.id = "navigation";
-        navBarLinks = `Window's inner height is ${window.innerHeight}`;
-
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        navBar.classList.add("navigation-scroll");
+        // myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        navBar.classList.remove("navigation-scroll");
+        // myNav.classList.remove("nav-colored");
     }
+};
+
+
+
+// function changeNavBarColor () {
+
+//     if (window.scrollY > 300) {
+
+//         navBar.id = "navigationWhite";
+//         navBarLinks.classList.remove("navbar-transparent");
+//         navBarLinks.classList.add("navbar-white");
+        
+//     } else {
+
+//         navBar.id = "navigation";
+//         navBarLinks.classList.remove("navbar-white");
+//         navBarLinks.classList.add("navbar-transparent");;
+
+//     }
     
-}
+// }
 
 // Create a function that changes the guitar images from black and white to color (use a transition effect)
 
 /* EVENT LISTENERS */
 
-window.addEventListener('mousewheel', changeNavBarColor);
+window.addEventListener('onScroll', changeNavBarColor);
