b006b2d76945fb0146de5108c1cbe89de0e465b2
Olly <75307864+ladeAdeyemi@users.noreply.github.com>
2021-05-02 14:54:24 +0100

    Added some more JavaScript to the homepage (specifically the SVG-Highlight, Guitars etc and Newsletter Sections)

--- javascript/main.js
+++ javascript/main.js
@@ -4,15 +4,30 @@ const navBar = document.querySelector('#navigation');
 const extraLinksContainer = document.querySelector('#navigation div#extra-links-container');
 const navBarLinks = document.querySelectorAll('#navigation div.nav-links-container a');
 const hamburger = document.querySelector('div.hamburger-hidden');
+const svgDivs = document.querySelectorAll('.svg-card');
+const imgContainers = document.querySelectorAll('div.image-container img');
+const signUpContainer = document.querySelector('#sign-up-container');
+
+/* EVENT LISTENERS */
+
+window.addEventListener('scroll', changeNavBarColor);
+window.addEventListener('scroll', changeNavLinksColor);
+window.addEventListener('scroll', showSvgDivs);
+window.addEventListener('scroll', increaseImgOpacity);
+window.addEventListener('scroll', signUpSlideOut);
+window.addEventListener('resize', hideDropDown);
+hamburger.addEventListener('click', showDropDownMenu);
 
 /* FUNCTIONS */
+
+// Navigation Bar Section
 
 function changeNavBarColor () {
 
     if (window.scrollY >= 200 ) {
 
         navBar.classList.add("navigationWhite");
-      
+    
     } else {
 
         navBar.classList.remove("navigationWhite");
@@ -40,6 +55,7 @@ function changeNavLinksColor () {
     });
 }
 
+
 function showDropDownMenu () {
 
         extraLinksContainer.classList.toggle('open');
@@ -56,9 +72,65 @@ function hideDropDown () {
 
 }
 
-/* EVENT LISTENERS */
+// SVG-Highlight Section
 
-window.addEventListener('scroll', changeNavBarColor);
-window.addEventListener('scroll', changeNavLinksColor);
-window.addEventListener('resize', hideDropDown);
-hamburger.addEventListener('click', showDropDownMenu);
+function showSvgDivs () {
+
+    if (window.scrollY >= 1650) {
+
+        Array.from(svgDivs).forEach((div) => {
+
+            setTimeout(div.classList.remove('hide'), 3000)
+            
+        });
+
+    }  else {
+
+        Array.from(svgDivs).forEach((div) => {
+
+            setTimeout(div.classList.add('hide'), 3000)
+            
+        });
+    }
+
+}
+
+// Guitars, Amps, Mics, Picks Section
+
+function increaseImgOpacity () {
+
+    const imagesInImageContainer = Array.from(imgContainers);
+    const [img1, img2, img3, img4] = imagesInImageContainer;
+
+    if (window.scrollY >= 2007) {
+        img1.parentNode.classList.remove('low-opacity');
+    }
+
+    if (window.scrollY >= 2350) {
+        img2.parentNode.classList.remove('low-opacity');
+    }
+
+    if (window.scrollY >= 3000) {
+        img3.parentNode.classList.remove('low-opacity');
+    }
+
+    if (window.scrollY >= 3550) {
+        img4.parentNode.classList.remove('low-opacity');
+    }
+
+}
+
+// Newsletter Section
+
+function signUpSlideOut () {
+
+    if (window.scrollY >= 5300) {
+
+        signUpContainer.classList.remove('hidden');
+
+    } else {
+        signUpContainer.classList.add('hidden');
+    }
+
+}
+
