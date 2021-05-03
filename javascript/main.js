2e4eddcf477d6b9148ba314049dca68f31fd9351
Olly <75307864+ladeAdeyemi@users.noreply.github.com>
2021-05-03 10:39:42 +0100

    Added some more JavaScript to the homepage including very minimal form validation in the newsletter section

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,36 @@
 /* DOM SELECTIONS */
 
+
 const navBar = document.querySelector('#navigation');
 const extraLinksContainer = document.querySelector('#navigation div#extra-links-container');
 const navBarLinks = document.querySelectorAll('#navigation div.nav-links-container a');
 const hamburger = document.querySelector('div.hamburger-hidden');
+const introTextContainer = document.querySelector('div.intro-text-container');
 const svgDivs = document.querySelectorAll('.svg-card');
 const imgContainers = document.querySelectorAll('div.image-container img');
+const brandsUls = document.querySelectorAll('section#brands div.brands-grid ul.brands-row');
 const signUpContainer = document.querySelector('#sign-up-container');
+const signUpForm = document.querySelector('#sign-up-container form');
+const signUpInput = document.querySelector('section#newsletter input');
+
 
 /* EVENT LISTENERS */
 
+
 window.addEventListener('scroll', changeNavBarColor);
 window.addEventListener('scroll', changeNavLinksColor);
+window.addEventListener('scroll', revealIntroText);
 window.addEventListener('scroll', showSvgDivs);
 window.addEventListener('scroll', increaseImgOpacity);
+window.addEventListener('scroll', addBoxShadow);
 window.addEventListener('scroll', signUpSlideOut);
 window.addEventListener('resize', hideDropDown);
 hamburger.addEventListener('click', showDropDownMenu);
+signUpForm.addEventListener('submit', validateForm);
+
 
 /* FUNCTIONS */
+
 
 // Navigation Bar Section
 
@@ -55,7 +67,6 @@ function changeNavLinksColor () {
     });
 }
 
-
 function showDropDownMenu () {
 
         extraLinksContainer.classList.toggle('open');
@@ -72,11 +83,27 @@ function hideDropDown () {
 
 }
 
+
+// Intro Section
+
+
+function revealIntroText () {
+
+    if (window.scrollY >= 600) {
+        introTextContainer.classList.remove('intro-text-hidden');
+    } else {
+        introTextContainer.classList.add('intro-text-hidden');
+    }
+
+}
+
+
 // SVG-Highlight Section
+
 
 function showSvgDivs () {
 
-    if (window.scrollY >= 1650) {
+    if (window.scrollY >= 1550) {
 
         Array.from(svgDivs).forEach((div) => {
 
@@ -95,7 +122,9 @@ function showSvgDivs () {
 
 }
 
+
 // Guitars, Amps, Mics, Picks Section
+
 
 function increaseImgOpacity () {
 
@@ -114,22 +143,78 @@ function increaseImgOpacity () {
         img3.parentNode.classList.remove('low-opacity');
     }
 
-    if (window.scrollY >= 3550) {
+    if (window.scrollY >= 3350) {
         img4.parentNode.classList.remove('low-opacity');
     }
 
 }
 
+
+// Brands Section
+
+
+function addBoxShadow () {
+
+    if (window.scrollY >= 4100) {
+       
+        brandsUls.forEach((ul) => {
+            ul.classList.add('raised');
+        });
+
+    } else {
+
+        brandsUls.forEach((ul) => ul.classList.remove('raised'));
+
+    }
+
+}
+
+
 // Newsletter Section
+
 
 function signUpSlideOut () {
 
-    if (window.scrollY >= 5300) {
+    if (window.scrollY >= 5200) {
 
         signUpContainer.classList.remove('hidden');
 
     } else {
         signUpContainer.classList.add('hidden');
+    }
+
+}
+
+function showValidationMessage (validationMessage, seconds) {
+
+        const messageDiv = document.createElement('div');
+        const messageParagraph = document.createElement('p');
+        messageDiv.className = 'messageContainer';
+        messageDiv.appendChild(messageParagraph);
+        signUpContainer.lastElementChild.appendChild(messageDiv);
+        messageParagraph.innerText = validationMessage;
+        setTimeout(() => signUpContainer.lastElementChild.removeChild(messageDiv), seconds);
+        // input.value = "";
+    
+}
+
+function validateForm (e) {
+
+    e.preventDefault();
+    
+    const validationMessages = ["Please fill in this field.", "Please enter a valid email address."];
+
+    if (signUpInput.value == "") {
+
+        showValidationMessage(validationMessages[0], 2000);
+        return;
+    }  
+
+    if (signUpInput.value.length < 1 && signUpInput.value !== "") {
+
+        showValidationMessage(validationMessages[1], 2000);
+        return;
+
     }
 
 }
